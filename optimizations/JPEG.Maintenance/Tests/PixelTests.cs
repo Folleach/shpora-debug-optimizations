098d2efce1f747d23e3e22e448127fe128c4bff2
Folleach <32067915+Folleach@users.noreply.github.com>
2021-04-03 20:11:00 +0500

    Remove unused variables in Pixel

---
+++ optimizations/JPEG.Maintenance/Tests/PixelTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using JPEG.Images;
+using JPEG.Maintenance.Legacy;
+using NUnit.Framework;
+
+namespace JPEG.Maintenance.Tests
+{
+    [TestFixture]
+    public class PixelTests
+    {
+        [Test, Combinatorial]
+        public void Pixel_Constructor_PixelShouldBeEquivalentWithLegacyPixel(
+            [Values(0, 100, 255)] int firstComponent,
+            [Values(13, 17, 0)] int secondComponent,
+            [Values(255, 0, 33)] int thirdComponent,
+            [Values(PixelFormat.RGB)] PixelFormat format)
+        {
+            var pixel = new Pixel(firstComponent, secondComponent, thirdComponent, format);
+            var legacyPixel = new Pixel_Legacy(firstComponent, secondComponent, thirdComponent, format);
+            
+            pixel.Should().BeEquivalentTo(legacyPixel);
+        }
+    }
+}
--- optimizations/JPEG/Images/Pixel.cs
+++ optimizations/JPEG/Images/Pixel.cs
@@ -14,34 +14,21 @@ namespace JPEG.Images
             if (!SupportFormats.Contains(pixelFormat))
                 throw new FormatException("Unknown pixel format: " + pixelFormat);
             format = pixelFormat;
-            if (pixelFormat == PixelFormat.RGB)
-            {
-                r = firstComponent;
-                g = secondComponent;
-                b = thirdComponent;
-            }
-            if (pixelFormat == PixelFormat.YCbCr)
-            {
-                y = firstComponent;
-                cb = secondComponent;
-                cr = thirdComponent;
-            }
+            first = firstComponent;
+            second = secondComponent;
+            third = thirdComponent;
         }
 
-        private readonly double r;
-        private readonly double g;
-        private readonly double b;
+        private readonly double first;
+        private readonly double second;
+        private readonly double third;
 
-        private readonly double y;
-        private readonly double cb;
-        private readonly double cr;
+        public double R => format == PixelFormat.RGB ? first : (298.082 * first + 408.583 * third) / 256.0 - 222.921;
+        public double G => format == PixelFormat.RGB ? second : (298.082 * first - 100.291 * second - 208.120 * third) / 256.0 + 135.576;
+        public double B => format == PixelFormat.RGB ? third : (298.082 * first + 516.412 * second) / 256.0 - 276.836;
 
-        public double R => format == PixelFormat.RGB ? r : (298.082 * y + 408.583 * Cr) / 256.0 - 222.921;
-        public double G => format == PixelFormat.RGB ? g : (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
-        public double B => format == PixelFormat.RGB ? b : (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
-
-        public double Y => format == PixelFormat.YCbCr ? y : 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
-        public double Cb => format == PixelFormat.YCbCr ? cb : 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
-        public double Cr => format == PixelFormat.YCbCr ? cr : 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
+        public double Y => format == PixelFormat.YCbCr ? first : 16.0 + (65.738 * first + 129.057 * second + 24.064 * third) / 256.0;
+        public double Cb => format == PixelFormat.YCbCr ? second : 128.0 + (-37.945 * first - 74.494 * second + 112.439 * third) / 256.0;
+        public double Cr => format == PixelFormat.YCbCr ? third : 128.0 + (112.439 * first - 94.154 * second - 18.285 * third) / 256.0;
     }
 }
