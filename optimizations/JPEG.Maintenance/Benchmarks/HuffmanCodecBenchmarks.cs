8c58bab4d1c9afeae51a97892fdca2c4428d7ad0
Folleach <32067915+Folleach@users.noreply.github.com>
2021-04-05 12:19:43 +0500

    GetNodes now is put the values to List

---
+++ optimizations/JPEG.Maintenance/Benchmarks/HuffmanCodecBenchmarks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using JPEG.Maintenance.Legacy;
+
+namespace JPEG.Maintenance.Benchmarks
+{
+    [DisassemblyDiagnoser()]
+    public class HuffmanCodecBenchmarks
+    {
+        private const int N = 30;
+        private byte[] data;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var random = new Random();
+            data = new byte[N];
+            for (var i = 0; i < N; i++)
+                data[i] = (byte)random.Next(20);
+        }
+        
+        [Benchmark]
+        public void HuffmanCodec_Encode()
+        {
+            HuffmanCodec.Encode(data, out _, out _);
+        }
+
+        [Benchmark]
+        public void HuffmanCodecLegacy_Encode()
+        {
+            HuffmanCodec_Legacy.Encode(data, out _, out _);
+        }
+    }
+}
--- optimizations/JPEG.Maintenance/Legacy/HuffmanCodec_Legacy.cs
+++ optimizations/JPEG.Maintenance/Legacy/HuffmanCodec_Legacy.cs
@@ -4,24 +4,24 @@ using System.Threading;
 using System.Threading.Tasks;
 using JPEG.Utilities;
 
-namespace JPEG
+namespace JPEG.Maintenance.Legacy
 {
-	class HuffmanNode
+    class HuffmanNode_Legacy
 	{
 		public byte? LeafLabel { get; set; }
 		public int Frequency { get; set; }
-		public HuffmanNode Left { get; set; }
-		public HuffmanNode Right { get; set; }
+		public HuffmanNode_Legacy Left { get; set; }
+		public HuffmanNode_Legacy Right { get; set; }
 	}
 
-	public class BitsWithLength
+	public class BitsWithLength_Legacy
 	{
 		public int Bits { get; set; }
 		public int BitsCount { get; set; }
 
-		public class Comparer : IEqualityComparer<BitsWithLength>
+		public class Comparer : IEqualityComparer<BitsWithLength_Legacy>
 		{
-			public bool Equals(BitsWithLength x, BitsWithLength y)
+			public bool Equals(BitsWithLength_Legacy x, BitsWithLength_Legacy y)
 			{
 				if(x == y) return true;
 				if(x == null || y == null)
@@ -29,24 +29,39 @@ namespace JPEG
 				return x.BitsCount == y.BitsCount && x.Bits == y.Bits;
 			}
 
-			public int GetHashCode(BitsWithLength obj)
+			public int GetHashCode(BitsWithLength_Legacy obj)
 			{
 				if(obj == null)
 					return 0;
 				return ((397 *obj.Bits) << 5) ^ (17* obj.BitsCount);
 			}
 		}
+		
+		public bool Equals(BitsWithLength_Legacy x, BitsWithLength_Legacy y)
+		{
+			if(x == y) return true;
+			if(x == null || y == null)
+				return false;
+			return x.BitsCount == y.BitsCount && x.Bits == y.Bits;
+		}
+
+		public int GetHashCode(BitsWithLength_Legacy obj)
+		{
+			if(obj == null)
+				return 0;
+			return ((397 *obj.Bits) << 5) ^ (17* obj.BitsCount);
+		}
 	}
 
-	class BitsBuffer
+	class BitsBuffer_Legacy
 	{
 		private List<byte> buffer = new List<byte>();
-		private BitsWithLength unfinishedBits = new BitsWithLength();
+		private BitsWithLength_Legacy unfinishedBits = new BitsWithLength_Legacy();
 
-		public void Add(BitsWithLength bitsWithLength)
+		public void Add(BitsWithLength_Legacy bitsWithLengthLegacy)
 		{
-			var bitsCount = bitsWithLength.BitsCount;
-			var bits = bitsWithLength.Bits;
+			var bitsCount = bitsWithLengthLegacy.BitsCount;
+			var bits = bitsWithLengthLegacy.Bits;
 
 			int neededBits = 8 - unfinishedBits.BitsCount;
 			while(bitsCount >= neededBits)
@@ -76,18 +91,18 @@ namespace JPEG
 		}
 	}
 
-	class HuffmanCodec
+	class HuffmanCodec_Legacy
 	{
-		public static byte[] Encode(IEnumerable<byte> data, out Dictionary<BitsWithLength, byte> decodeTable, out long bitsCount)
+		public static byte[] Encode(IEnumerable<byte> data, out Dictionary<BitsWithLength_Legacy, byte> decodeTable, out long bitsCount)
 		{
 			var frequences = CalcFrequences(data);
 
 			var root = BuildHuffmanTree(frequences);
 
-			var encodeTable = new BitsWithLength[byte.MaxValue + 1];
+			var encodeTable = new BitsWithLength_Legacy[byte.MaxValue + 1];
 			FillEncodeTable(root, encodeTable);
 
-			var bitsBuffer = new BitsBuffer();
+			var bitsBuffer = new BitsBuffer_Legacy();
 			foreach(var b in data)
 				bitsBuffer.Add(encodeTable[b]);
 
@@ -96,12 +111,12 @@ namespace JPEG
 			return bitsBuffer.ToArray(out bitsCount);
 		}
 
-		public static byte[] Decode(byte[] encodedData, Dictionary<BitsWithLength, byte> decodeTable, long bitsCount)
+		public static byte[] Decode(byte[] encodedData, Dictionary<BitsWithLength_Legacy, byte> decodeTable, long bitsCount)
 		{
 			var result = new List<byte>();
 
 			byte decodedByte;
-			var sample = new BitsWithLength { Bits = 0, BitsCount = 0 };
+			var sample = new BitsWithLength_Legacy { Bits = 0, BitsCount = 0 };
 			for(var byteNum = 0; byteNum < encodedData.Length; byteNum++)
 			{
 				var b = encodedData[byteNum];
@@ -109,7 +124,7 @@ namespace JPEG
 				{
 					sample.Bits = (sample.Bits << 1) + ((b & (1 << (8 - bitNum - 1))) != 0 ? 1 : 0);
 					sample.BitsCount++;
-
+	
 					if(decodeTable.TryGetValue(sample, out decodedByte))
 					{
 						result.Add(decodedByte);
@@ -122,9 +137,9 @@ namespace JPEG
 			return result.ToArray();
 		}
 
-		private static Dictionary<BitsWithLength, byte> CreateDecodeTable(BitsWithLength[] encodeTable)
+		private static Dictionary<BitsWithLength_Legacy, byte> CreateDecodeTable(BitsWithLength_Legacy[] encodeTable)
 		{
-			var result = new Dictionary<BitsWithLength, byte>(new BitsWithLength.Comparer());
+			var result = new Dictionary<BitsWithLength_Legacy, byte>(new BitsWithLength_Legacy.Comparer());
 			for(int b = 0; b < encodeTable.Length; b++)
 			{
 				var bitsWithLength = encodeTable[b];
@@ -136,21 +151,21 @@ namespace JPEG
 			return result;
 		}
 
-		private static void FillEncodeTable(HuffmanNode node, BitsWithLength[] encodeSubstitutionTable, int bitvector = 0, int depth = 0)
+		private static void FillEncodeTable(HuffmanNode_Legacy nodeLegacy, BitsWithLength_Legacy[] encodeSubstitutionTable, int bitvector = 0, int depth = 0)
 		{
-			if(node.LeafLabel != null)
-				encodeSubstitutionTable[node.LeafLabel.Value] = new BitsWithLength {Bits = bitvector, BitsCount = depth};
+			if(nodeLegacy.LeafLabel != null)
+				encodeSubstitutionTable[nodeLegacy.LeafLabel.Value] = new BitsWithLength_Legacy {Bits = bitvector, BitsCount = depth};
 			else
 			{
-				if(node.Left != null)
+				if(nodeLegacy.Left != null)
 				{
-					FillEncodeTable(node.Left, encodeSubstitutionTable, (bitvector << 1) + 1, depth + 1);
-					FillEncodeTable(node.Right, encodeSubstitutionTable, (bitvector << 1) + 0, depth + 1);
+					FillEncodeTable(nodeLegacy.Left, encodeSubstitutionTable, (bitvector << 1) + 1, depth + 1);
+					FillEncodeTable(nodeLegacy.Right, encodeSubstitutionTable, (bitvector << 1) + 0, depth + 1);
 				}
 			}
 		}
 
-		private static HuffmanNode BuildHuffmanTree(int[] frequences)
+		private static HuffmanNode_Legacy BuildHuffmanTree(int[] frequences)
 		{
 			var nodes = GetNodes(frequences);
 			
@@ -160,22 +175,17 @@ namespace JPEG
 				nodes = nodes.Without(firstMin);
 				var secondMin = nodes.MinOrDefault(node => node.Frequency);
 				nodes = nodes.Without(secondMin);
-				nodes = nodes.Concat(new HuffmanNode {Frequency = firstMin.Frequency + secondMin.Frequency, Left = secondMin, Right = firstMin }.ToEnumerable());
+				nodes = nodes.Concat(new HuffmanNode_Legacy {Frequency = firstMin.Frequency + secondMin.Frequency, Left = secondMin, Right = firstMin }.ToEnumerable());
 			}
 			return nodes.First();
 		}
 
-		private static IEnumerable<HuffmanNode> GetNodes(int[] frequences)
+		private static IEnumerable<HuffmanNode_Legacy> GetNodes(int[] frequences)
 		{
-			var nodes = new List<HuffmanNode>();
-			for (var i = 0; i < byte.MaxValue + 1; i++)
-			{
-				if (frequences[i] <= 0)
-					continue;
-				nodes.Add(new HuffmanNode() {Frequency = frequences[i], LeafLabel = (byte)i});
-			}
-
-			return nodes;
+			return Enumerable.Range(0, byte.MaxValue + 1)
+				.Select(num => new HuffmanNode_Legacy {Frequency = frequences[num], LeafLabel = (byte) num})
+				.Where(node => node.Frequency > 0)
+				.ToArray();
 		}
 
 		private static int[] CalcFrequences(IEnumerable<byte> data)
